6997651d211902ce5f9ac76803dc555cd3921b0a
blaccerik <jerikkalle@gmail.com>
2023-09-17 16:59:49 +0300

    fixed merge bugs

--- transfer_all.py
+++ transfer_all.py
@@ -10,24 +10,26 @@ def main():
     ip = "52.174.181.107"
     name = "erik"
 
+    # todo add current dir not lups1 or lups2
+
     commands = [
 
-        # (r"C:\Users\erik\PycharmProjects\lups\data",
+        # (r"C:\Users\erik\PycharmProjects\lups2\data",
         #  f"{name}@{ip}:/var/www/data"),
-      
-        (r"C:\Users\erik\PycharmProjects\lups\celeryr",
-         f"{name}@{ip}:/var/www/"),
-
-        (r"C:\Users\erik\PycharmProjects\lups\flaskr",
-         f"{name}@{ip}:/var/www/"),
-
-        (r"C:\Users\erik\PycharmProjects\lups\docker-compose.yaml",
-         f"{name}@{ip}:/var/www/docker-compose.yaml"),
-
-        (r"C:\Users\erik\WebstormProjects\lups\lups\dist\lups\*",
+        #
+        # (r"C:\Users\erik\PycharmProjects\lups2\celeryr",
+        #  f"{name}@{ip}:/var/www/"),
+        #
+        # (r"C:\Users\erik\PycharmProjects\lups2\flaskr",
+        #  f"{name}@{ip}:/var/www/"),
+        #
+        # (r"C:\Users\erik\PycharmProjects\lups2\docker-compose.yaml",
+        #  f"{name}@{ip}:/var/www/docker-compose.yaml"),
+        #
+        (r"C:\Users\erik\WebstormProjects\lups3\lups\dist\lups\*",
           f"{name}@{ip}:/var/www/html/"),
 
-        (r"C:\Users\erik\PycharmProjects\lups\nginx\nginx.conf",
+        (r"C:\Users\erik\PycharmProjects\lups2\nginx\nginx.conf",
          f"{name}@{ip}:/etc/nginx/nginx.conf"),
     ]
 
