d54937c3ea4bd1ff3730da539d53a93dcfa5f4da
erik <wajtkassikack@gmail.com>
2023-09-25 20:07:07 +0300

    docker fix

--- lups/src/environments/environment.prod.ts
+++ lups/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lyps.ee/api', // Use your production server URL here
+  apiUrl: '/api', // Use your production server URL here
 };
--- transfer_all.py
+++ transfer_all.py
@@ -6,8 +6,8 @@ import os
 #  f"{name}@{ip}:/var/www/html"),
 
 def main():
-    key = r"C:\Users\erik\desktop\erikfinal.pem"
-    ip = "52.174.181.107"
+    key = r"C:\Users\erik\.ssh\digital"
+    ip = "165.232.82.30"
     name = "erik"
 
     # todo add current dir not lups1 or lups2
@@ -17,8 +17,8 @@ def main():
         # (r"C:\Users\erik\PycharmProjects\lups2\data",
         #  f"{name}@{ip}:/var/www/data"),
         #
-        # (r"C:\Users\erik\PycharmProjects\lups2\celeryr",
-        #  f"{name}@{ip}:/var/www/"),
+        (r"C:\Users\erik\PycharmProjects\lups\celeryr\t5-model",
+         f"{name}@{ip}:/home/erik/lups/celeryr"),
         #
         # (r"C:\Users\erik\PycharmProjects\lups2\flaskr",
         #  f"{name}@{ip}:/var/www/"),
@@ -26,11 +26,11 @@ def main():
         # (r"C:\Users\erik\PycharmProjects\lups2\docker-compose.yaml",
         #  f"{name}@{ip}:/var/www/docker-compose.yaml"),
         #
-        (r"C:\Users\erik\WebstormProjects\lups3\lups\dist\lups\*",
-          f"{name}@{ip}:/var/www/html/"),
-
-        (r"C:\Users\erik\PycharmProjects\lups2\nginx\nginx.conf",
-         f"{name}@{ip}:/etc/nginx/nginx.conf"),
+        # (r"C:\Users\erik\WebstormProjects\lups3\lups\dist\lups\*",
+        #   f"{name}@{ip}:/var/www/html/"),
+        #
+        # (r"C:\Users\erik\PycharmProjects\lups2\nginx\nginx.conf",
+        #  f"{name}@{ip}:/etc/nginx/nginx.conf"),
     ]
 
     for s, e in commands:
