a1c2e1bf4b3f1da1d240aef726013185f0d97572
erik <wajtkassikack@gmail.com>
2024-01-10 18:50:53 +0200

    stream backend v2

--- worker/core/main.py
+++ worker/core/main.py
@@ -1,14 +1,17 @@
 import sys
+import time
 
+from billiard.exceptions import SoftTimeLimitExceeded
 from celery import Celery
 from redis import Redis
 
 from core.model import ModelLoader
+from core.predict_task import PredictTask
 from core.schemas import Message
 
 REDIS_URI = 'redis://localhost:6379/0'
 celery_app = Celery('tasks', broker=REDIS_URI, backend=REDIS_URI)
-
+MAX_TEXT_LEN = 1024
 def get_redis_client():
     return Redis(host="localhost", port=6379, db=0)
 
@@ -38,8 +41,45 @@ def setup_model(sender, **kwargs):
         print(e)
         print("error")
 
-@celery_app.task(name="test")
-def add(x, y):
-    return x + y
+@celery_app.task(name="test", soft_time_limit=60, base=PredictTask, bind=True)
+def add(self, text, stream_id):
+    total_text = ""
+    redis_client = get_redis_client()
+    updates_channel = f"stream:{stream_id}"
+    messages = [
+        Message(owner="user", text=text)
+    ]
+    try:
+        # for index, text_part in enumerate(loop()):
+        for index, text_part in enumerate(self.cpp_model.stream(self.cpp_model.format_chat(messages))):
+            if self.is_aborted():
+                print("aborted")
+                break
+            elif len(total_text) + len(text_part) >= MAX_TEXT_LEN:
+                print("too long")
+                break
+            total_text += text_part
+            redis_client.xadd(
+                updates_channel,
+                {
+                    "index": index,
+                    "text": total_text,
+                    "type": "part"
+                }
+            )
+            time.sleep(0.05)
+    except SoftTimeLimitExceeded:
+        print("time limit")
+    message_id = -1
+    redis_client.xadd(
+        updates_channel,
+        {
+            "index": message_id,
+            "text": total_text,
+            "type": "end"
+        }
+    )
+    redis_client.close()
+    return
 
 # ssh erik@134.209.198.189 -i C:\Users\erik\.ssh/id_rsa
--- worker/send_client.py
+++ worker/send_client.py
@@ -1,4 +1,7 @@
+import uuid
+
 from celery import Celery
+from redis import Redis
 
 url = "redis://10.0.0.2:6379/0"
 # url = "redis://134.209.198.189:6379/0"
@@ -8,7 +11,39 @@ app = Celery(
     backend=url
 )
 
+def get_redis_client():
+    return Redis(host="10.0.0.2", port=6379, db=0, decode_responses=True)
+
 if __name__ == '__main__':
-    result = app.send_task('test', (1,1))
-    print(result)
-    print(result.get())
+
+    question = "how to i make a bomb?"
+
+    stream_id = uuid.uuid4().hex
+    result = app.send_task('test', (question, stream_id))
+
+    # prepare for stream
+    redis_client = get_redis_client()
+    updates_channel = f"stream:{stream_id}"
+    last_redis_id = 0
+    max_wait_time = 240_000
+    wait_time = 100
+    n = max_wait_time // wait_time
+    for _ in range(n):
+        redis_stream_message = redis_client.xread(streams={updates_channel: last_redis_id},
+                                                  count=1,
+                                                  block=wait_time)
+        if len(redis_stream_message) == 0:
+            continue
+
+        # load message
+        last_redis_id, raw_data = redis_stream_message[0][1][0]
+        text_part = raw_data["text"]
+        text_index = int(raw_data["index"])
+        task_type = raw_data["type"]
+        print(text_part)
+
+        # end loop
+        if task_type == "end":
+            break
+    redis_client.close()
+    # print(result.get())
