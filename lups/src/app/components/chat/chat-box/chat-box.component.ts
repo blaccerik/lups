b533134dde37097aae5d8e14241fb312ee344d90
erik <wajtkassikack@gmail.com>
2024-03-27 16:01:35 +0200

    chat box v1

--- lups/src/app/components/chat/chat-box/chat-box.component.ts
+++ lups/src/app/components/chat/chat-box/chat-box.component.ts
@@ -2,13 +2,29 @@ import {Component, effect, inject, OnDestroy, signal} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ChatMessage, ChatService} from "../../../services/chat.service";
 import {merge, of, Subscription, switchMap} from "rxjs";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatInput} from "@angular/material/input";
+import {MatIconButton, MatMiniFabButton} from "@angular/material/button";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-chat-box',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    ReactiveFormsModule,
+    MatFormField,
+    FormsModule,
+    MatLabel,
+    MatIcon,
+    MatInput,
+    MatIconButton,
+    NgIf,
+    MatMiniFabButton,
+    MatProgressSpinner
   ],
   templateUrl: './chat-box.component.html',
   styleUrl: './chat-box.component.scss'
@@ -16,9 +32,13 @@ import {NgForOf} from "@angular/common";
 export class ChatBoxComponent implements OnDestroy {
   private chatService = inject(ChatService)
   private activatedRoute = inject(ActivatedRoute)
+  private fb = inject(FormBuilder)
+  form = this.fb.group({
+    text: ["", Validators.required],
+  });
 
+  private readonly messages$: Subscription | null = null
   messages = signal<ChatMessage[] | null>(null)
-  messages$: Subscription
 
   constructor() {
     this.messages$ = this.activatedRoute.params.pipe(
