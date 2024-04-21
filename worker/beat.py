a6809a2a33c6c15040e72b124247e83c22adbac3
erik <wajtkassikack@gmail.com>
2024-04-21 19:34:00 +0300

    docker env

--- worker/beat.py
+++ worker/beat.py
@@ -10,6 +10,6 @@ celery_app = Celery('main_app',
 celery_app.conf.beat_schedule = {
     'run-task': {
         'task': 'news',
-        'schedule': crontab(minute='25', hour='19')
+        'schedule': crontab(minute='37', hour='19')
     },
 }
