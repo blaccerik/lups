bf64be4a1c2c5390b7d3a6fba1193375c0e0003e
erik <jerikkalle@gmail.com>
2024-05-03 17:13:13 +0300

    yt v2

--- music_worker/test.py
+++ music_worker/test.py
@@ -7,11 +7,11 @@ from task.music_task2 import download_by_song_id
 
 
 def main1():
-    random.seed(11)
+    random.seed(69)
 
     # generate data
     db = {}
-    amount = 100_000
+    amount = 3000
     connections = 50
     for i in range(amount):
         for _ in range(connections):
@@ -37,6 +37,15 @@ def main1():
         print(len(result), nr, te - ts)
     t2 = time.time()
     print("took", t2 - t1)
+    # results:
+    # 40 6360 0.36654186248779297
+    # 40 6687 0.30622100830078125
+    # 40 7041 0.3215463161468506
+    # 40 7617 0.34729719161987305
+    # 40 11045 0.5045084953308105
+    # 0 100001 3.6011691093444824
+    # 0 100001 3.6106362342834473
+    # load data to memory and search it in there
     return
 
 def query(banned, db):
