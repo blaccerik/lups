9f1bf2163a439abdb881674d47e5bf7e9eb40a03
erik <wajtkassikack@gmail.com>
2024-05-22 17:33:27 +0300

    frontend structure

---
+++ music/src/app/services/google-api.service.ts
@@ -0,0 +1,58 @@
+import {Injectable, signal} from '@angular/core';
+import {OAuthService} from "angular-oauth2-oidc";
+
+
+export interface UserInfo {
+  info: {
+    sub: string,
+    name: string,
+    picture: string
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleApiService {
+
+  userinfo = signal<UserInfo>({
+    info: {
+      sub: "",
+      name: "",
+      picture: ""
+    }
+  })
+
+  constructor(private oAuthService: OAuthService) {
+    oAuthService.configure({
+      issuer: 'https://accounts.google.com',
+      strictDiscoveryDocumentValidation: false,
+      redirectUri: window.location.origin,
+      clientId: '437646142767-evt2pt3tn4pbrjcea6pd71quq07h82j7.apps.googleusercontent.com',
+      scope: 'openid profile email',
+      showDebugInformation: true,
+    });
+    oAuthService.loadDiscoveryDocument().then((e) => {
+      oAuthService.tryLoginImplicitFlow().then(() => {
+        if (!oAuthService.hasValidAccessToken()) {
+          oAuthService.initLoginFlow()
+        } else {
+          oAuthService.loadUserProfile().then((userProfile) => {
+            this.userinfo.set(userProfile as UserInfo)
+          })
+        }
+      })
+    })
+  }
+
+  isLoggedIn(): boolean {
+    return this.oAuthService.hasValidAccessToken();
+  }
+
+  signOut() {
+    this.oAuthService.revokeTokenAndLogout().then(() => {
+        this.oAuthService.initLoginFlow()
+      }
+    )
+  }
+}
